f0066394e492a809bf128cff28fb1f76f6474da3
Githubsoniaryan3 <151424994+ARYANSONIII@users.noreply.github.com>
2025-10-27 20:17:27 +0530

    Restore MIT License and update start function

--- STORM/modules/start.py
+++ STORM/modules/start.py
@@ -55,14 +55,15 @@ async def start(event):
         bot_name = KEX.first_name
         bot_id = KEX.id
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **🍁 ᴀɴɪᴍᴀʟ 🍁: [🍁 ᴊᴀɴᴡᴀʀ 🍁](https://t.me/smartness_to_hai)**\n"
-        TEXT += f"» **ғᴜᴄᴋ :** `M3.0` \n"
-        TEXT += f"» **ᴘʏᴛʜᴏɴ :** `3.11` \n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ :** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
+        TEXT += f"» **🍁 ᴀɴɪᴍᴀʟ 🍁: [ᴊᴀɴᴡᴀʀ](https://t.me/smartness_to_hai)**\n"
+        TEXT += f"» **🍁 ғᴜᴄᴋ 🍁:** `M3.0` \n"
+        TEXT += f"» **🍁 ᴘʏᴛʜᴏɴ 🍁:** `3.11` \n"
+        TEXT += f"» **🍁 ᴛᴇʟᴇᴛʜᴏɴ 🍁:** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
         await event.client.send_file(
                     event.chat_id,  
                     "https://te.legra.ph/file/ec19cf227791a167abedc.jpg",
                     caption=TEXT, 
                     buttons=START_OP
                 )
+
 
